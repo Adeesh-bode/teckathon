37c41cb4b30bbc8e4b85f5689d44603954502aa7
Satyam Urkude <satyamurkude@gmail.com>
2024-03-30 12:09:24 +0530

    Updated

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,4 @@
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 
 const app = initializeApp({
   apiKey: import.meta.env.VITE_API_KEY,
@@ -7,7 +7,7 @@ const app = initializeApp({
   projectId: import.meta.env.VITE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID
+  appId: import.meta.env.VITE_APP_ID,
 });
 
 export default app;
